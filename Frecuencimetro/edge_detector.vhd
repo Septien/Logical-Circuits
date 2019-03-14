9e2ef1f55ba3edef29f370c0590911b20c11a765
Jose Septien <jaseptienh@gmail.com>
2019-03-14 10:23:49 -0600

    Add habilitator to edge detector.

--- Frecuencimetro/edge_detector.vhd
+++ Frecuencimetro/edge_detector.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity EdgeDet is
   port(
   clk, rst  : in std_logic;
-  Fx        : in std_logic;
+  Fx, h        : in std_logic;
   pulse     : out std_logic
 );
 end EdgeDet;
@@ -14,15 +14,17 @@ architecture detector of EdgeDet is
 begin
   process(clk, rst, Fx, R0, R1)
   begin
-    if (rst = '0') then
+	 if (h = '1') then
+		if (rst = '0') then
+			R0 <= '0';
+			R1 <= '0';
+		elsif (rising_edge(clk)) then
+			R0 <= Fx;
+			R1 <= R0;
+		end if;
+	else
       R0 <= '0';
       R1 <= '0';
-    elsif (rising_edge(clk)) then
-      R0 <= Fx;
-      R1 <= R0;
-    else
-      R0 <= R0;
-      R1 <= R1;
     end if;
   end process;
     
