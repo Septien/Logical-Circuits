9a5a50f074235d324529f482dd133473c62116ba
Jose Septien <jaseptienh@gmail.com>
2019-03-20 08:57:05 -0600

    Add more test cases in pwm bench.

--- PWM/pwm_tb.vhd
+++ PWM/pwm_tb.vhd
@@ -21,5 +21,5 @@ begin
   pulse : PWM port map(clk, rst, kSel, p);
   clk <= not clk after 20 ns;
   rst <= '0', '1' after 20 ns;
-  kSel <= "0000";
+  kSel <= "0000", "0001" after 2 ms, "0010" after 4 ms, "0011" after 6 ms, "0100" after 8 ms, "0101" after 10 ms, "0110" after 12 ms, "0111" after 14 ms, "1000" after 16 ms, "1001" after 18 ms, "1011" after 20 ms;
 end bench;
